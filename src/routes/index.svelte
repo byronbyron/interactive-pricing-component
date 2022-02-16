11a11044e2e2a7722b34a6f5cc21be0f831253fd
Byronn Walker-Mills <bwalkermills@gmail.com>
2022-02-16 10:53:19 +0000

    Add html markup and toggle switch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Simple traffic-based pricing</h1>
+<p>Sign-up for our 30-day trial. No credit card required.</p>
+
+<form>
+  <p>Pageviews</p>
+
+  <p>$<span>16</span> /month</p>
+
+  <div>
+    <span>Monthly Billing</span>
+
+    <!-- Rounded switch -->
+    <label class="switch">
+      <input type="checkbox">
+      <span class="slider round"></span>
+    </label>
+
+    <span>Yearly Billing</span>
+
+    <span>25% discount</span>
+  </div>
+
+  <div class="form-footer">
+    <ul>
+      <li>Unlimited websites</li>
+      <li>100% data ownership</li>
+      <li>Email reports</li>
+    </ul>
+
+    <button>Start my trial</button>
+  </div>
+</form>
+
+<div class="attribution">
+  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+  Coded by <a href="#">Your Name Here</a>.
+</div>
